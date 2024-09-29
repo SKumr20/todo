07f98b7dccaa90c029a89c8c3b80b79e7ebb57c9
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2024-09-30 00:46:32 +0530

    Add input popup

--- src/index.js
+++ src/index.js
@@ -3,17 +3,43 @@ import './styles.css';
 import { createTask } from './tasks';
 
 let taskId = 1;
-let indicator = document.getElementById('indicator');
-indicator.innerHTML = 0;
 
-document.getElementById('addTaskBtn').addEventListener('click', function () {
-    // prompt user for task
-    const taskName = prompt('Task name: ');
+//get all the elements
 
-    // if user provides a name, create the task
+const indicator = document.getElementById('indicator');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taksInputPopup = document.getElementById('taskInput');
+const enterBtn = document.getElementById('enterBtn');
+const cancelBtn = document.getElementById('cancelBtn');
+const taskNameInput = document.getElementById('taskName');
+
+
+indicator.innerHTML = taskId;
+
+addTaskBtn.addEventListener('click', () => {
+    taksInputPopup.classList.remove('hidden');
+});
+
+enterBtn.addEventListener('click', () => {
+    const taskName = taskNameInput.value.trim();
+
     if (taskName) {
         createTask(taskName, taskId);
         taskId++;
-        indicator.innerHTML = taskId-1;
+        indicator.innerHTML = taskId;
+        
+        // reset the values
+        taskNameInput.value = '';
+        taksInputPopup.classList.add('hidden');
     }
 });
+
+// Hide popup for cancel button
+
+cancelBtn.addEventListener('click', () => {
+    taskNameInput.value = '';
+    taksInputPopup.classList.add('hidden');
+});
+
+// Initial loaded task
+createTask('Be Happy', 1);
