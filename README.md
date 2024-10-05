a6ca4b04b2a6a8daac9bd9cea57d9e02df78c290
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2024-10-05 22:16:36 +0530

    Add due date and time functions Add due date amd time indicator for each task

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## Tools used
 
-* HTML, Javascript, Webpack, TailWindCSS, Webpack, Flowbite components
+* HTML, Javascript, Webpack, TailWindCSS, Webpack, Flowbite-UI components
 
 ## [Link](https://skumr20.github.io/todo/)
 
@@ -11,3 +11,4 @@
 
 1. Add Multiple tasks
 2. Dynamic Task Indicator
+3. Due date and time selector
--- src/time.js
+++ src/time.js
@@ -1,10 +1,31 @@
 export function timeModal(taskId) {
     const timeInputModal = document.getElementById('timeInputModal');
+    const enterTimeBtn = document.getElementById('enterTimeBtn');
+    const dateInput = document.getElementById('datepicker-actions');
+    const timeInput = document.getElementById('time');
+
 
     timeInputModal.classList.remove('hidden');
 
     const cancelBtn = document.getElementById('cancelTimeBtn');
     cancelBtn.addEventListener('click', () => {
         timeInputModal.classList.add('hidden');
+    });
+
+    enterTimeBtn.addEventListener('click', () => {
+        const dateSelect = dateInput.value;
+        const timeSelect = timeInput.value;
+
+        if (dateSelect && timeSelect) {
+            const taskLabel = document.querySelector(`label[for="task${taskId}"]`);
+            const timeSpan = document.createElement('div');
+            timeSpan.className = 'text-xs font-light tracking-tight text-gray-500';
+            timeSpan.textContent = `${dateSelect} at ${timeSelect}`;
+
+            taskLabel.innerHTML = taskLabel.textContent.replace(/\(Due on.*?\)/, '');
+            taskLabel.appendChild(timeSpan);
+
+            timeInputModal.classList.add('hidden');
+        }
     })
 }
